bb22352c8f9fb2bcf4fb7e5a8ac0518680c7f442
arekbednarz1 <arkadiusz_bednarz@o2.pl>
2021-04-22 19:43:13 +0200

    podstawowa implementacja interfejsow guardRepo oraz adminRepo

--- src/main/java/pl/lsab/prisonmanagerproject/repository/AdminRepository.java
+++ src/main/java/pl/lsab/prisonmanagerproject/repository/AdminRepository.java
@@ -1,4 +1,12 @@
 package pl.lsab.prisonmanagerproject.repository;
 
-public class AdminRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.lsab.prisonmanagerproject.entity.Admin;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin,Long> {
+
+    Admin findAdminByPasswordBefore(String password);
+
 }
