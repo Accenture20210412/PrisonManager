c52abf76afee060f6f01d9c70b0aeaf690f160ec
arekbednarz1 <arkadiusz_bednarz@o2.pl>
2021-04-22 17:18:03 +0200

    klasa entity Admin

--- src/main/java/pl/lsab/prisonmanagerproject/entity/Admin.java
+++ src/main/java/pl/lsab/prisonmanagerproject/entity/Admin.java
@@ -1,14 +1,39 @@
 package pl.lsab.prisonmanagerproject.entity;
 
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
-import lombok.ToString;
-import javax.persistence.Entity;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Size(min = 3, message = "imię powinno zawierać minimum 3 znaki")
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @Size(min = 3, message = "nazwisko powinno zawierać minimum 3 znaki")
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @Size(min = 5, message = "Minimum 5 znaków")
+    @NotNull
+    @NotEmpty
+    private String password;
+
+    @Transient
+    private String passwordRepeat;
 
 }
