1e2ad876a713c251793280f59c701eb471cc709b
Leszek <leszek.szczotka@o2.pl>
2021-04-22 19:27:41 +0200

    commit testowy

--- src/main/java/pl/lsab/prisonmanagerproject/PrisonManagerProjectApplication.java
+++ src/main/java/pl/lsab/prisonmanagerproject/PrisonManagerProjectApplication.java
@@ -2,6 +2,9 @@ package pl.lsab.prisonmanagerproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
 
 @SpringBootApplication
 public class PrisonManagerProjectApplication {
--- src/main/java/pl/lsab/prisonmanagerproject/entity/Prisoner.java
+++ src/main/java/pl/lsab/prisonmanagerproject/entity/Prisoner.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "osadzeni")
+@Table
 @Getter
 @Setter
 @NoArgsConstructor
