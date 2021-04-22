699c6f79bb77e3885f1ebb054dd71fde035548d2
arekbednarz1 <arkadiusz_bednarz@o2.pl>
2021-04-22 17:08:38 +0200

    dodano lombok w pom

--- src/main/java/pl/lsab/prisonmanagerproject/entity/Admin.java
+++ src/main/java/pl/lsab/prisonmanagerproject/entity/Admin.java
@@ -1,4 +1,14 @@
 package pl.lsab.prisonmanagerproject.entity;
 
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
 public class Admin {
+
 }
