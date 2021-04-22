1e23077d88d7da495fe756af6e64be284e22f7f9
arekbednarz1 <arkadiusz_bednarz@o2.pl>
2021-04-22 16:12:17 +0200

    F

---
+++ src/main/java/pl/lsab/prisonmanagerproject/PrisonManagerProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.lsab.prisonmanagerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrisonManagerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrisonManagerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lsab/prisonmanagerproject/PrisonManagerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lsab.prisonmanagerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrisonManagerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
