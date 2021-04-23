b3f654c77bcbdd89b68b6cff46daddf57e63b685
Leszek <leszek.szczotka@o2.pl>
2021-04-23 09:52:34 +0200

    dodanie encji cela oraz zaleznosci

---
+++ src/main/java/pl/lsab/prisonmanagerproject/entity/Cell.java
@@ -0,0 +1,31 @@
+package pl.lsab.prisonmanagerproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cell {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "numer celi")
+    private int cellNumber;
+
+    @Column(name = "stan celi")
+    private boolean isItEmpty;
+
+    @OneToMany
+    private Set<Prisoner> prisoners;
+}
